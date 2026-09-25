1f79fd47c3b13aa4c398a0c3c8b4ba107ad5da4d
urbanstylefashionng-commits <urbanstylefashionng@gmail.com>
2026-09-25 11:43:28 +0100

    Create send-email.js

---
+++ api/send-email.js
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "urban-website-83665",
+  "private_key_id": "fda3d1b6cf1420e46b3857d88b298cc73a2001c1",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC8C5odHieqAa7K\nLTzKiBuD+7XH5R2l1VpW4zqMZKMobxi98RyBDWOTBrVtgQB3/+wzJhB1/cyLM40D\nr1og1lRwmraLvTA0NdKWtZWOI8hZbmJFr0RUbBkH99hwWM0uLcSIHRwM/2EmO3WV\nEKxJxtNUfk2787x7fJuDs2c7SrxMU03IFviCtkDyhzMK2mUEuSOUOGbKRMWsXD3o\noZ685fgnygLVk69lNiOzj/JlQYZxUHeRx6Nzsdk+dvxKVgQw01RjZge4VOv0im9l\ne3vnBzvkycn2Ex0MirwCcMb+RMGQmt0+evg1Idca8CA6gjnMJVTw6ZMCPKlei13g\nNKvOwoorAgMBAAECggEAQ0j7ehPCKYi0nE8Xg3i0L8OvRblrpFz1GEXyOy3/W4Z1\nGfHeOycpTACIhog0PLvsLlkeZrnLqLtnZ7wkyb8nM6uISfQF/YiMs7SBsPyIwcdH\njpuqHXMYectrPLfpou5pdjLESLpQbrez2QTAQR3NnFwdQHV0KdNqafOVqpeRTNWc\n5QapKAzg1fwOTgBIoToY9kmzy61kp2vuoK9llGzItj/Kdcb1yc9bv7hwHrN6YRQV\nwiOtMHRO1l5KXzxmC9zGNdwS2XZZtEhylOYFHF3Huj6CGrTb1A0nbAcdq5DZOsRW\n3+FTZQBSv/a+FrTTMlucL+j1bkaWPJh+h0WNKUqCAQKBgQDelgc6AsLBYAAFhEhp\nYobFSDIOYClv9+2OsRercR4CZUZrlNadgNf1bqZDjc+shQKcW5mhLJpmv9d185cb\nE140lXLEP4W5PAC7zS4qybHXU9KWSeHk888RHnVg0y2S/0Jh667sGPMieW6Lis4I\n1lPnHQEit1SWyc4o5pp0wTPYgQKBgQDYRizYsUXpLCl26HdRLu3DzC5oxlG9n9Ch\nPJUJ+7DvVFXzX7oVWu8jsJ3KcuIWImGQH4XEDN3bkqHf7XokbzGisMb/uh9nAXBd\nckLbwRksq/ipMKsniWfw641tFQhp5OLS6vQvf7muAxu54AifiPJN+kdxuFvOr+n/\nVQqAQIrsqwKBgQCZx5FKlOm06/xX02Kr3HQ8DHr4z5sRTnsDoHo36jjPcuTKItR7\nkK6jVETuWyVljaeWGvrzDXvHDTdwuFBnVmQ5dB8MUs6ygTAIj+MbkKmbmL/fMUAu\nC/u9WgvCT2QnMM/OEySQ3VPtN3QWWFmsZVcmTzWfpIV+gAI848VGcMy/AQKBgAMh\nuQKGRqHivtL2+MtXJ6A/M6jWvlZFmyrKisnzOBOvH/N/Pl2SzIWyyAYndbimVM1W\nd4RoP0ovrGJmuO8QISLe9nK7ijrenXbs8aTm/etUriwdBU10QuoNLyYWOOH3q4Us\nW87YRaMMbnlZkiZlQET6y9AMwF+U0omc/KLwzFIzAoGAZsRf4sSvRzxD6FEt1PRD\nZt8OzIwevDKNxkTjLdPSGcvfoPA5Vg04JPfgq7XLmHsQbR7uT+G423dDAmT83kCC\niQoBNfKaSmdxiV/BS+2Emdhfst8ZZcZv8bUbi8ERALg6vUxWm00WNw4aLDwe7ZFV\ntljNQPclknZcEQNk8vpzF+I=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@urban-website-83665.iam.gserviceaccount.com",
+  "client_id": "113898629352099486171",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40urban-website-83665.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
